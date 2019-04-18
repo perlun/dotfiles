perlun/dotfiles

commit 8623f8f4c444ecf372bf44866c1b1388c41ad3e9

bash_aliases: convert restart_tomcat to function instead of alias

bash_aliases
@@ -27,7 +27,6 @@ alias kill_tomcat='pkill -f tomcat-8.*catalina\.startup'
 alias kill_tomcat_dlx='pkill -f tomcat-dlx.*catalina\.startup'
 alias start_tomcat='$HOME/java/tomcat-8/bin/startup.sh'
 alias restart_tomcat='kill_tomcat ; sleep 1 ; start_tomcat'
-alias restart_tomcat_dlx='kill_tomcat_dlx ; sleep 1 ; $HOME/java/tomcat-dlx/bin/startup.sh'
 
 alias kill_gradle='pkill -e -f gradle'
 alias tail_tomcat_log='tail -n30 -f ~/java/tomcat-8/logs/catalina.out | rainbow -f catalina'
@@ -41,6 +40,13 @@ alias virsh='sudo virsh'
 # VisualVM doesn't play well with https://github.com/perlun/perlun-cinnamon-theme,
 # so forcing the Metal look-and-feel makes it behave more reasonably.
 alias visualvm='visualvm --laf Metal --jdkhome /usr/lib/jvm/java-11-openjdk-amd64'
+
+# Quirk: has to be a function so that "./gw devBuild && restart_tomcat" can work
+restart_tomcat() {
+    kill_tomcat
+    sleep 1
+    start_tomcat
+}
 
 # Git aliases (technically, functions)
 gbranches() {

commit c9322d01e6fb39ed0f8a54ef4298482ba2588381

bash_aliases: fix bug in restart_tomcat
The bug was introduced in 8623f8f4; I inadvertently removed the wrong
alias, causing `restart_tomcat` to be defined both as an alias and a
function. This made the function definition not work.

bash_aliases
@@ -26,7 +26,6 @@ alias pbcopy="xclip -sel clip"
 alias kill_tomcat='pkill -f tomcat-8.*catalina\.startup'
 alias kill_tomcat_dlx='pkill -f tomcat-dlx.*catalina\.startup'
 alias start_tomcat='$HOME/java/tomcat-8/bin/startup.sh'
-alias restart_tomcat='kill_tomcat ; sleep 1 ; start_tomcat'
 
 alias kill_gradle='pkill -e -f gradle'
 alias tail_tomcat_log='tail -n30 -f ~/java/tomcat-8/logs/catalina.out | rainbow -f catalina'