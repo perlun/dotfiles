d9ca6c4e538de14183b993f5628c2a4101586da9
Per Lundberg <perlun@gmail.com>
2023-05-05 21:00:13 +0300

    (install.sh) Make it even better
    - Set up git customization automatically
    - Set up nano customization automatically

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+#
+# Bash customizations
+#
 rm -f ~/.profile && ln -sf ~/git/dotfiles/profile ~/.profile
 
 # Gets created by default on e.g. msys2, takes precedence over `~/.profile` so must be deleted.
@@ -7,3 +10,21 @@ rm -f ~/.bash_profile
 
 rm -f ~/.bashrc && ln -sf ~/git/dotfiles/bashrc ~/.bashrc
 rm -f ~/.bash_aliases && ln -sf ~/git/dotfiles/bash_aliases ~/.bash_aliases
+
+#
+# Nano customizations
+#
+rm -f ~/.nanorc && ln -sf git/dotfiles/nanorc ~/.nanorc
+
+#
+# Git customizations
+#
+if [ ! -f ~/.gitconfig ]; then
+  cat >> ~/.gitconfig <<EOF
+[include]
+        path = ~/git/dotfiles/gitconfig
+
+[user]
+        email = perlun@gmail.com
+EOF
+fi
